3139c62378b6b0ea84501458b78396665df374bb
supahgreg <greg.parris@gmail.com>
2010-12-12 22:22:22 -0600

    first commit

---
+++ bootstrap.js
@@ -0,0 +1,49 @@
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+Cu.import("resource://gre/modules/Services.jsm");
+
+let cleanupAry = [];
+
+function abQuit() {
+  Cc['@mozilla.org/toolkit/app-startup;1'].getService(Ci.nsIAppStartup)
+      .quit(Ci.nsIAppStartup.eAttemptQuit);
+}
+
+function main(win) {
+  let appButton = win.document.getElementById("appmenu-button");
+  appButton.addEventListener("dblclick", abQuit, true);
+
+  let idx1 = cleanupAry.push(function() {
+    appButton.removeEventListener("dblclick", abQuit, true);
+  }) - 1;
+  let idx2 = cleanupAry.push(function() (
+      win.removeEventListener("unload", winUnloader, false))) - 1;
+  function winUnloader() {
+    cleanupAry[idx1] = null;
+    cleanupAry[idx2] = null;
+  }
+  win.addEventListener("unload", winUnloader, false);
+}
+
+function install(){}
+function uninstall(){}
+function startup() {
+  let browserWins = Services.wm.getEnumerator("navigator:browser");
+  while (browserWins.hasMoreElements()) main(browserWins.getNext());
+
+  function winObs(aSubject, aTopic) {
+    if ("domwindowopened" != aTopic) return;
+    let winLoad = function() {
+      aSubject.removeEventListener("load", winLoad, false);
+      if ("navigator:browser" ==
+          aSubject.document.documentElement.getAttribute("windowtype"))
+        main(aSubject);
+    }
+    aSubject.addEventListener("load", winLoad, false);
+  }
+  Services.ww.registerNotification(winObs);
+  cleanupAry.push(function() Services.ww.unregisterNotification(winObs));
+}
+function shutdown(data, reason) {
+  if (reason !== APP_SHUTDOWN)
+    for (let [, cleaner] in Iterator(cleanupAry)) cleaner && cleaner();
+}
